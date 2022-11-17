2707675b6af00f4b47ef91a097c9e4678f814261
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-11-17 11:52:45 -0300

    Initial commit

---
+++ Assets/scripts/EnemyDataSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ENEMYDATA", menuName = "SO Enemy data")]
+public class EnemyDataSO : ScriptableObject
+{
+    public bool defeated;
+    public Transform tr;
+
+    public void RotateMe()
+    {
+        tr.Rotate(0, 0, 45);
+    }
+}
---
+++ Assets/scripts/RecuperarDatoPlayerPrefs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecuperarDatoPlayerPrefs : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(PlayerPrefs.GetString("nombreAlmacenado"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
